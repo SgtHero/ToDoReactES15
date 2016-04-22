6528a7282fa62ebbe6714bda3ac141a684d40f85
SgtHero <futuress@gmx.net>
2016-04-22 15:30:44 +0200

    did the tutorial and some testing

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,22 +1,27 @@
 import React, { Component, PropTypes } from 'react';
-
-import { Tasks } from '../api/tasks.js';
+import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
 
 export default class Task extends Component {
     toggleChecked() {
-        Tasks.update(this.props.task._id, {
-            $set: { checked: !this.props.task.checked }
-        });
+        Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
     }
 
     deleteThisTask() {
-        Tasks.remove(this.props.task._id);
+        Meteor.call('tasks.remove', this.props.task._id);
+    }
+
+    togglePrivate() {
+        Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
     }
     
     render() {
-        
-        const taskClassName= this.props.task.checked ? 'checked' : '';
-        
+
+        const taskClassName = classnames({
+            checked: this.props.task.checked,
+            private: this.props.task.private,
+        });
+
         return (
             <li className={taskClassName}>
                 <button className="delete" onClick={this.deleteThisTask.bind(this)}>
@@ -24,6 +29,10 @@ export default class Task extends Component {
                 </button>
                 
                 <input type="checkbox" readOnly checked={this.props.task.checked} onClick={this.toggleChecked.bind(this)}/>
+
+                { this.props.showPrivateButton ? (
+                    <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>{this.props.task.private ? 'Privat' : 'Public'}</button>
+                ) : ''}
                 
                 <span className="text">
                     <strong>{this.props.task.username}</strong>: {this.props.task.text}
@@ -34,5 +43,6 @@ export default class Task extends Component {
 }
 
 Task.propTypes = {
-    task: PropTypes.object.isRequired
+    task: PropTypes.object.isRequired,
+    showPrivateButton: React.PropTypes.bool.isRequired,
 };
